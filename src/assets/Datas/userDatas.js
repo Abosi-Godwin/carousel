4262ed4be922be4ced572e51124db9be1f46c5a5
Abosi-Godwin <bbnl6060@github.com>
2024-10-11 07:16:53 +0100

    Images moved to public folder

--- src/assets/Datas/userDatas.js
+++ src/assets/Datas/userDatas.js
@@ -2,31 +2,31 @@ export const users = [
     {
         name: "James Wilson",
         proff: "Web developer",
-        img: "/assets/SliderImages/img-1.jpg"
+        img: "../../public/SliderImages/img-1.jpg"
     },
     {
         name: "Sarah Johnson",
         proff: "graphic designer",
-        img: "/src/assets/SliderImages/img-2.jpg"
+        img: "../../public/SliderImages/img-2.jpg"
     },
     {
         name: "Michael Brown",
         proff: "project manager",
-        img: "../src/assets/SliderImages/img-3.jpg"
+        img: "../../public/SliderImages/img-3.jpg"
     },
     {
         name: "Emily Davis",
         proff: "marketing specialist",
-        img: "../src/assets/SliderImages/img-4.jpg"
+        img: "../../public/SliderImages/img-4.jpg"
     },
     {
         name: "Christopher Garcia",
         proff: "Data scientist",
-        img: "../src/assets/SliderImages/img-5.jpg"
+        img: "../../public/SliderImages/img-5.jpg"
     },
     {
         name: "Richard Wilson",
         proff: "product designer",
-        img: "../src/assets/SliderImages/img-6.jpg"
+        img: "../../public/SliderImages/img-6.jpg"
     }
 ];
